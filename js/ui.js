aa84b5374ee0147138488119524de521d9412eba
ziyang <leeziyang@naver.com>
2019-02-07 02:58:17 +0900

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,7 +1,19 @@
 window.onload = function (){
 
-
+var main = document.querySelector("main")
+var portfolio = document.querySelector(".portfolio");
 var liElems = document.querySelectorAll(".portfolio li");
+
+//포트폴리오 화면전환
+portfolio.addEventListener("click", function(ev){
+    ev.preventDefault();
+    
+    main.style.transform = "translateY(-640px)"
+
+});
+
+
+
 
 //새 창 팝업열기
 liElems.forEach(function(item){
@@ -15,6 +27,6 @@ liElems.forEach(function(item){
 
         window.open(address,"width= 1500, height=1000");
     })
-})
+});
 
-}// window
+};// window
